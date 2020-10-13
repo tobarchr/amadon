0eee75fdd164e9ec7b5eb492604e71dd6c5d5973
Chris <tobarchr@gmail.com>
2020-10-12 22:03:11 -0400

    changes to my repo

--- poorly_coded_store/urls.py
+++ poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('checkout/', views.checkout)
+    path('checkout', views.checkout),
+    path('checkout_success',views.checkout_success)
 ]
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Order, Product
 
 def index(request):
@@ -6,11 +6,26 @@ def index(request):
         "all_products": Product.objects.all()
     }
     return render(request, "store/index.html", context)
-
-def checkout(request):
+def checkout_success(request):
+    product_id = Product.objects.get(id=request.POST['product_id'])
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
+    price_from_form = float(product_id.price)
+    total_charge = quantity_from_form * price_from_form  
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('/checkout')
+def checkout(request):
+    all_orders= Order.objects.all()
+    last_order = Order.objects.last()
+    total_spent = 0
+    total_quantity = 0
+    for invoices in all_orders:
+        invoices.total_price
+        invoices.quantity_ordered
+        total_spent+=invoices.total_price
+        total_quantity += invoices.quantity_ordered
+    context = {
+    "total_spent" : total_spent,
+    "last_order" : last_order,
+    "total_quantity" : total_quantity
+    }
+    return render(request, "store/checkout.html",context)
